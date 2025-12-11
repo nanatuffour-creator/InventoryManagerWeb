b73f08d4d536f390759e4fd65285d861fe6dc028
nanatuffour-creator <tuffour_n@yahoo.com>
2025-12-11 15:24:40 +0000

    Getting all purchases

--- src/pages/purchases/purchases.ts
+++ src/pages/purchases/purchases.ts
@@ -1,15 +1,15 @@
 import { Component, inject, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SupplierService } from '../../Services/SupplierService/supplier-service';
 import { Supplier } from '../../Interfaces/suppliers';
-import { Customer } from '../../Interfaces/customers';
 import { ProductServices } from '../../Services/ProductServices/product-services';
 import { CustomerService } from '../../Services/customer-service';
 import { ProductsInterface } from '../../Interfaces/products';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { Purchase, Status } from '../../Interfaces/purchases';
 
 @Component({
   selector: 'app-purchases',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './purchases.html',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './purchases.css',
@@ -19,12 +19,58 @@ export class Purchases implements OnInit {
   suppliers: Supplier[] = [];
   ngOnInit(): void {
     this.getSupplier();
-    this.allCustomers();
     this.getProd();
+    this.getPurchase();
   }
-  // purchase : Purchases
+  today = new Date().toISOString().split('T')[0];
+  purchased: Purchase = {
+    purchaseId: 0,
+    id: 0,
+    amount: 0,
+    date: this.today,
+    stat: undefined,
+    purchaseOrders: [
+      {
+        purchaseItemId: 0,
+        costPrice: 0,
+        quantity: 0,
+        purchaseId: 0,
+        productId: 0,
+      },
+    ],
+  };
+
+  Status = Status;
   url = 'http://localhost:5251/api/Purchase/add';
   url1 = 'http://localhost:5251/api/Purchase/all';
+
+  addPurchase() {
+    // debugger
+    this.customerService.http.post(this.url, this.purchased).subscribe({
+      next(value) {
+        // debugger
+        console.log(value);
+        alert('Purchase Added Successfully');
+      },
+      error(err) {
+        // debugger
+        console.log(err);
+      },
+    });
+  }
+  allpurchases: any;
+  getPurchase() {
+    this.customerService.http.get(this.url1).subscribe({
+      next: (value) => {
+        this.allpurchases = value;
+        console.log(value);
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+  }
+
   getSupplier() {
     this.supplierService.getSuppliers().subscribe({
       next: (value: Supplier[]) => {
@@ -38,66 +84,52 @@ export class Purchases implements OnInit {
   }
   private productServices = inject(ProductServices);
   private customerService = inject(CustomerService);
-  private fb = inject(FormBuilder);
-  get items(): FormArray {
-    return this.invoiceForm.get('items') as FormArray;
-  }
+
   productsList: ProductsInterface[] = [];
-  customer: Customer[] = [];
-  removeItem(index: number) {
-    this.items.removeAt(index);
-  }
-  allCustomers() {
-    this.customerService.getCustomer().subscribe({
-      next: (value: Customer[]) => (this.customer = value),
-      error: (err) => console.log(err),
-    });
-  }
+
   getProd() {
     this.productServices.getProducts().subscribe({
       next: (response: ProductsInterface[]) => (this.productsList = response),
       error: (err) => console.error(err),
     });
   }
-  invoiceForm: FormGroup = this.fb.group({
-    customerId: ['', Validators.required],
-    items: this.fb.array([]), // FormArray of line items
-    // total : new FormControl(),
-  });
-  updatePrice(index: number) {
-    const row = this.items.at(index);
-    const productId = row.get('productId')!.value;
-    const product = this.productsList.find((p) => p.productId === productId);
-    if (!product) return;
 
-    row.patchValue({
-      sellingPrice: product.price ?? 0,
-      // productPrice: product.buyingPrice ?? 0,
+  orderTotals: number[] = [];
+  overallTotal: number = 0;
+
+  constructor() {
+    this.recalculateTotals();
+  }
+
+  addOrder() {
+    this.purchased.purchaseOrders.push({
+      purchaseItemId: 0,
+      costPrice: 0,
+      quantity: 0,
+      purchaseId: 0,
+      productId: 0,
     });
 
-    this.recalculateLine(index);
+    this.recalculateTotals();
   }
-  recalculateLine(index: number) {
-    const row = this.items.at(index);
-    const qty = +row.get('quantity')!.value || 0;
-    const price = +row.get('sellingPrice')!.value || 0;
-    row.patchValue({ lineTotal: qty * price }, { emitEvent: false });
+
+  removeOrder(index: number) {
+    this.purchased.purchaseOrders.splice(index, 1);
+    this.recalculateTotals();
   }
-  closeInvoiceModal() {
-    this.showInvoiceModal = false;
-  }
-  showInvoiceModal: boolean = false;
-  openInvoiceModal() {
-    this.showInvoiceModal = true;
-  }
-  addItem() {
-    const group = this.fb.group({
-      productId: ['', Validators.required],
-      quantity: [1, [Validators.required, Validators.min(1)]],
-      sellingPrice: [0, Validators.required],
-      productPrice: [0],
-      lineTotal: [0],
+
+  recalculateTotals() {
+    this.orderTotals = this.purchased.purchaseOrders.map((po) => {
+      const cost = po.costPrice || 0;
+      const qty = po.quantity || 0;
+      return cost * qty;
     });
-    this.items.push(group);
+
+    this.overallTotal = this.orderTotals.reduce((a, b) => a + b, 0);
+    this.purchased.amount = this.overallTotal;
+  }
+
+  savePurchase() {
+    console.log('Saved purchase:', this.purchased);
   }
 }
